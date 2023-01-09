be32fb8f7d496445add9c619967fb94cc8af1c0b
ArunRaj2312 <arunvijay1043534@gmail.com>
2023-01-09 00:09:17 -1200

    add file

---
+++ main.js
@@ -0,0 +1,31 @@
+//expression
+myFunc1=(a=1-(22/2*5))=> a+20;
+console.log(myFunc1());
+
+myFunc2=(a=20%2*4-2)=> a-54;
+console.log(myFunc2());
+
+myFunc3=(a=70/(22-2*5))=> a/40;
+console.log(myFunc3());
+
+myFunc4=(a=1+(22%2+5)/10)=> a+10;
+console.log(myFunc4());
+
+myFunc5=(a=8/(4+2*5))=> a%27;
+console.log(myFunc5());
+
+myFunc6=(a=72/8*10+2)=> a+76;
+console.log(myFunc6());
+
+myFunc7=(a=2*(67%3-5))=> a+20;
+console.log(myFunc7());
+
+myFunc8=(a=8-(78/4*10))=> a*67;
+console.log(myFunc8());
+
+myFunc9=(a=5-(200/2*50))=> a-10;
+console.log(myFunc9());
+
+myFunc10=(a=22/2*5+10)=> a/2;
+console.log(myFunc10());
+
